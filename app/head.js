9ceeb6891d41e3e7eb987727532c166fc1259a71
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2023-01-14 10:42:32 +0530

    font from next optomization

--- app/head.js
+++ app/head.js
@@ -4,12 +4,6 @@ export default function head() {
   return (
     <head>
       <title>Anomm</title>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700&display=swap"
-        rel="stylesheet"
-      />
     </head>
   );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,22 @@
 import React from "react";
 import "../styles/tailwind.css";
 import "../styles/custom.css";
-
+// using a custom font
+import { Manrope } from "@next/font/google";
+const manrope = Manrope({
+  subsets: ["latin"],
+  // default, can also use "swap" to ensure custom font always shows
+  display: "optional",
+});
 export default function RootLayout({
   // Layouts must accept a children prop.
   // This will be populated with nested layouts or pages
   children,
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={manrope.className}>
       <head></head>
-      <body className="custom-font">{children}</body>
+      <body>{children}</body>
     </html>
   );
 }

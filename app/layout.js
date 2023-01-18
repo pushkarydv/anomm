a979d72582a94d104517e37cfe1786c200373cb7
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2023-01-18 10:18:06 +0530

    fixed mobile layout

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,11 @@ const manrope = Manrope({
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={manrope.className}>
-      <head></head>
+      <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body>{children}</body>
     </html>
   );

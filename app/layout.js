e624cecce752a73bda79125e2570fc070bbaba53
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2023-01-21 03:17:34 +0530

    eslint fixes

--- app/layout.js
+++ app/layout.js
@@ -11,8 +11,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={manrope.className}>
       <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body>{children}</body>

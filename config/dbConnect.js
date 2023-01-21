a8a5533a95bb27fcf3536b79f23a654c0a9e896f
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2023-01-21 09:18:03 +0530

    monor fix

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -11,10 +11,6 @@ const options = {
 let client;
 let clientPromise;
 
-if (!process.env.MONGO_DB_URI) {
-  throw new Error("Please add your Mongo URI to .env.local");
-}
-
 if (process.env.NODE_ENV === "development") {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).

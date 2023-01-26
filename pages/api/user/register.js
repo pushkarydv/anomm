ed48808a0ffa7cc9ec13f52e03079869e683d047
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2023-01-26 15:40:35 +0530

    user register

--- pages/api/user/register.js
+++ pages/api/user/register.js
@@ -2,22 +2,46 @@ import axios from "axios";
 // import clientPromise from "../../../config/dbConnect";
 
 export default async (req, res) => {
-  try {
-    const updates = await axios.get(
-      `https://api.telegram.org/bot${process.env.NEXT_PUBLIC_TELEGRAM_BOT_API_TOKEN}/getUpdates`
-    );
-    // const client = await clientPromise;
-    // const db = client.db("users");
-    // const { username, userKey } = req.query;
+  const { username } = req.query;
+  const updates = await axios.get(
+    `https://api.telegram.org/bot${process.env.NEXT_PUBLIC_TELEGRAM_BOT_TOKEN}/getUpdates`
+  );
 
-    // const user = await db.collection("users").insertOne({
-    //   username,
-    //   userKey,
-    // });
-    res.setHeader("Cache-Control", `max-age=N, public`);
-    res.json(JSON.stringify(updates));
-  } catch (e) {
-    console.error(e);
-    throw new Error(e).message;
+  if (username) {
+    try {
+      let noMatchFlag = true;
+      for (let i = updates.data.result.length - 1; i >= 0; i--) {
+        if (
+          updates.data.result[i].message.from.username == username &&
+          updates.data.result[i].message.text == "/start"
+        ) {
+          const respond_to_user = await axios.get(
+            `https://api.telegram.org/bot${
+              process.env.NEXT_PUBLIC_TELEGRAM_BOT_TOKEN
+            }/sendMessage?chat_id=${
+              updates.data.result[i].message.from.id
+            }&text=${encodeURI(
+              "Hello, Welcome to Anomm.\nThis is a free service to deliver anonymous messages directly to your telegram via a bot to make you a friendly experience of chat based interface.\n\nManage Your account here: https://anomm.verce.app/dashbaord"
+            )}`
+          );
+          res.status(200).send(respond_to_user.data);
+          noMatchFlag = false;
+          break;
+        }
+      }
+      if (noMatchFlag) {
+        res.status(400).send({
+          error:
+            "sorry your username was not connected to bot please connect by going to bot and pressing start buttor, or else what you can do is type /start in bot",
+        });
+      }
+    } catch {
+      res.status(400).send({
+        error:
+          "sorry your username was not connected to bot please connect by going to bot and pressing start buttor, or else what you can do is type /start in bot",
+      });
+    }
+  } else {
+    res.status(400).send({ error: "username not in query" });
   }
 };

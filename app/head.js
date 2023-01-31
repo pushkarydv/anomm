b277ee15074bcf5171cbb56e85bc89510300f947
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2023-01-31 23:00:35 +0530

    head ssr fixes

--- app/head.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function head() {
-  return (
-    <head>
-      <title>Anomm</title>
-    </head>
-  );
-}
--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={manrope.className}>
       <head>
+        <title>Anomm</title>
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
